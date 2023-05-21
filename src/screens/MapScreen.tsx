346933a366392e7aa7d8064bb6f8bde638e749a8
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-05-21 17:26:25 -0600

    IOS configuracion GoogleMaps

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export const MapScreen = () => {
   return (
     <View style={{flex:1}}>
        <MapView
+      //  provider={PROVIDER_GOOGLE} Para que en ambos se vea el de Google maps
         style={{flex:1}}
         initialRegion={{
           latitude: 37.78825,
