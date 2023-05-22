2cbd17f779564cd574cfd507d2bed8be219b91db
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-05-22 08:52:36 -0600

    Obtener Coordenadas de usuario

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MapView, { Marker } from 'react-native-maps'
+import Geolocation from '@react-native-community/geolocation';
+
 
 export const Map = () => {
+    
+    useEffect(() => {
+      Geolocation.getCurrentPosition(
+        info => console.log(info),
+        (err)=> console.log({err}),
+        {
+             enableHighAccuracy:true
+        }
+
+        );
+    
+  }, [])
+  
   return (
     <>
         <MapView
       //  provider={PROVIDER_GOOGLE} Para que en ambos se vea el de Google maps
+        showsUserLocation 
         style={{flex:1}}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 37.33023256,
+          longitude: -122.02344987,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
