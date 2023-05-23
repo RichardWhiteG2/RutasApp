d263a4c7b756a73a330b5f509d9cfabcf231472b
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-05-22 18:20:19 -0600

    Evitar cambios de state

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -15,37 +15,48 @@ import { Location } from "../interfaces/appInterfaces";
         longitude:0 
     })
 
-    const watchId = useRef<number >();
+    const watchId = useRef<number >(); 
+    const  isMounted = useRef(true);
+
+    useEffect(() => {
+      isMounted.current=true;
+      return () => {
+          isMounted.current=false ;
+      }
+    }, [ ])
+    
     useEffect(() => {
  
         getCurrentLocation().then( location => {
+            if(!isMounted.current)return; 
             setInitialPosition(location); 
             setUserLocation(location);
             setRouteLines(routes=>[...routes, location ]); 
             setHasLocation(true);
         });
     }, []);
-
+    
     const getCurrentLocation = (): Promise<Location>=>{
         return new Promise((resolve, reject)=>{
             Geolocation.getCurrentPosition(
                 ({coords}) => {
-                  resolve({
-                      latitude: coords.latitude,
-                      longitude: coords.longitude
-                  })
+                    resolve({
+                        latitude: coords.latitude,
+                        longitude: coords.longitude
+                    })
                 },
                 (err)=> reject({err}),{ enableHighAccuracy:true }
                 ); 
-        });
-    }
-
-    const followUserLocation = ()=>{
-        watchId.current  = Geolocation.watchPosition(
-            ({coords}) => { 
-                const location:  Location ={
-                    latitude: coords.latitude,
-                    longitude: coords.longitude
+            });
+        }
+        
+        const followUserLocation = ()=>{
+            watchId.current  = Geolocation.watchPosition(
+                ({coords}) => { 
+                    if(!isMounted.current)return;  
+                    const location:  Location ={
+                        latitude: coords.latitude,
+                        longitude: coords.longitude
                 } 
                 setUserLocation(location);
                 setRouteLines(routes=>[...routes , location ]);  
